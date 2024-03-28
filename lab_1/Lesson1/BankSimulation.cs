e29d6218e4c20942c1da4ce9782ac4418bcbb0b7
Yosin <ziyoev2612@gmail.com>
2024-03-29 04:55:56 +0700

    Initial commit

---
+++ lab_1/Lesson1/BankSimulation.cs
@@ -0,0 +1,111 @@
+﻿using Lesson1.TypeOfBankAccount;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    internal class BankSimulation
+    {
+        public List<Person> Client { get; set; } = null;
+        public BankSimulation() 
+        {
+
+            Client = GetClient();
+            SimulationTransactions();
+        }
+
+        public void PerformMonthEndTransactions()
+        {
+            foreach (var client in Client)
+            {
+                foreach (var account in client.BankAccounts)
+                    account.PerformMonthEndTransactions();
+            }
+        }
+        public string GetClientsAccountsHistory()
+        {
+            var report = new StringBuilder();
+            int k = 0;
+            report.AppendLine(" № Client\t\tTypeOfBankAccount");
+            foreach (var client in Client)
+            {
+                report.AppendLine((++k).ToString() + "\t\t" + client.Name);
+                int t = 0;
+                foreach (var account in client.BankAccounts)
+                    report.AppendLine((++t).ToString()+"\t\t" + TypeDescriptor.GetClassName(account).ToString()+"\n" + account.GetAccountHistory()+"\n");
+            }
+            return report.ToString();
+
+
+
+
+        }
+        private List<Person> GetClient()
+        {
+            return new List<Person>() {
+        new Person("Kryukova Yana"){
+                        BankAccounts = new List<BankAccount>()
+                                       {new InterestEarningAccount("Kryukova Yana",10000m),
+                                        new LineOfCreditAccount("Kryukova Yana")}},
+        new Person("Ivanov Ivan"){
+        BankAccounts = new List<BankAccount>(){ new InterestEarningAccount("Ivanov Ivan",10000m),
+                                        new LineOfCreditAccount("Ivanov Ivan"),
+                                        new GiftCardAccount("Ivanov Ivan",2000m)}}
+            };
+        }
+
+        private void SimulationTransactions()
+        {
+            var person = Client.Find((p) => p.Name == "Kryukova Yana");
+            var deposit = person!= null ? person.BankAccounts.Find((p) => p is InterestEarningAccount):null;
+            if (deposit != null)
+            {
+                deposit.MakeDeposit(750, DateTime.Now, "save some money");
+                deposit.MakeDeposit(1250, DateTime.Now, "Add more savings");
+                deposit.MakeWithdraw(250, DateTime.Now, "Needed to pay monthly bills");
+            }
+            var lineOfCredit = person != null ? person.BankAccounts.Find((p) => p is LineOfCreditAccount) : null;
+            if (lineOfCredit != null)
+            {
+                lineOfCredit.MakeWithdraw(1000m, DateTime.Now, "Take out monthly advance");
+                lineOfCredit.MakeDeposit(50m, DateTime.Now, "Pay back small amount");
+                lineOfCredit.MakeWithdraw(5000m, DateTime.Now, "Emergency funds for repairs");
+                lineOfCredit.MakeDeposit(150m, DateTime.Now, "Partial restoration on repairs");
+            }
+            person = Client.Find((p) => p.Name == "Ivanov Ivan");
+            deposit = person != null ? person.BankAccounts.Find((p) => p is InterestEarningAccount) : null;
+            if (deposit != null)
+            {
+                deposit.MakeDeposit(750, DateTime.Now, "save some money");
+                deposit.MakeDeposit(1250, DateTime.Now, "Add more savings");
+                deposit.MakeWithdraw(250, DateTime.Now, "Needed to pay monthly bills");
+            }
+            lineOfCredit = person != null ? person.BankAccounts.Find((p) => p is LineOfCreditAccount) : null;
+            if (lineOfCredit != null)
+            {
+                lineOfCredit.MakeWithdraw(1000m, DateTime.Now, "Take out monthly advance");
+                lineOfCredit.MakeDeposit(50m, DateTime.Now, "Pay back small amount");
+                lineOfCredit.MakeWithdraw(5000m, DateTime.Now, "Emergency funds for repairs");
+                lineOfCredit.MakeDeposit(150m, DateTime.Now, "Partial restoration on repairs");
+            }
+            var giftCard = person != null ? person.BankAccounts.Find((p) => p is GiftCardAccount) : null;
+            if (lineOfCredit != null)
+            {
+                giftCard.MakeWithdraw(20, DateTime.Now, "get expensive coffee");
+                giftCard.MakeWithdraw(50, DateTime.Now, "buy groceries");
+            }
+
+
+           
+
+        }
+
+
+
+
+    }
+}
---
+++ lab_1/Lesson1/TypeOfBankAccount/BankAccount.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1.TypeOfBankAccount
+{
+   abstract class BankAccount {
+        private static int _accountNumberSeed = 1000000000;
+        private readonly decimal _minimumBalance;
+        private List<Transaction> _allTransactions = new List<Transaction>();
+        // Публичное автоматическое свойство
+        // Тип строка имя Number,
+        // Можем только получить номер счета, но не можем установить
+        public string Number { get; }
+
+        // Баланс кошелька
+        public decimal Balance { get
+            {
+                decimal balance = 0m;
+                foreach (var item in _allTransactions)
+                    balance += item.Amount;
+                return balance;
+            }
+        }
+
+        public string Owner { get; set; }
+        public DateTime Date { get; }
+
+        // Конструктор класса
+        /// <summary>
+        /// Открытие нового счета
+        /// </summary>
+        /// <param name="name">Имя владельца счета</param>
+        /// <param name="initalBalance">Начальный баланс, по умолчанию 0</param>
+        public BankAccount(string name, decimal initalBalance = 0m):this(name, 0,initalBalance)
+        {
+        }
+        public BankAccount(string name, decimal minimumBalance, decimal initalBalance = 0m)
+        {
+            //this.Owner = name;
+            Owner = name;
+            if (initalBalance < 0)
+                throw new ArgumentOutOfRangeException(nameof(initalBalance), "Balance must be positive");
+            MakeDeposit(initalBalance, DateTime.Now, "Initial balance");
+            //Number = Convert.ToString(_accountNumberSeed);
+            //Number = (_accountNumberSeed++).ToString();
+            Number = _accountNumberSeed.ToString();
+            _accountNumberSeed++;
+            if (minimumBalance < 0)
+                throw new ArgumentOutOfRangeException(nameof(minimumBalance), "Balance must be positive");
+            _minimumBalance = minimumBalance;
+
+        }
+
+            public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount < 0)
+                throw new ArgumentOutOfRangeException
+                    (nameof(amount), "Amount of deposit must be positive");
+            var deposit = new Transaction(amount, date, note);
+            _allTransactions.Add(deposit);
+        }
+        public void MakeWithdraw(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+
+            Transaction? overdraftTransaction = CheckWithdrawalLimit(Balance - amount < -_minimumBalance);
+            _allTransactions.Add(new(-amount, date, note));
+            if (overdraftTransaction != null)
+                _allTransactions.Add(overdraftTransaction);
+        }
+
+        protected virtual Transaction? CheckWithdrawalLimit(bool isOverdrawn)
+        {
+            if (isOverdrawn)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            else
+            {
+                return default;
+            }
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+            decimal balance = 0m;
+            report.AppendLine("Date\t\tAmount\tBalance\tNote");
+            foreach (var item in _allTransactions)
+            {
+                balance += item.Amount;
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
+            }
+            return report.ToString();
+        }
+        public abstract void PerformMonthEndTransactions();
+
+    }
+}
