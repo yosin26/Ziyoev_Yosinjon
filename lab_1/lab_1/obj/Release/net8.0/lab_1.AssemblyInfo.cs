f5bc7ac3d2744ad9d3eaefcb2547587e5492906e
ziyoe <ziyoev2612@gmail.com>
2024-03-28 21:21:11 +0700

    Пример коммент

--- lab_1/lab_1/obj/Release/net8.0/lab_1.AssemblyInfo.cs
+++ lab_1/lab_1/obj/Release/net8.0/lab_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3c6a5d6ecffa22300243fc37df1420dba0d4b58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77d9a7833d98ec74c60a6f0075dbb9c4717e93e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
