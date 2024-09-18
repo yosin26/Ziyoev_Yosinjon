21a27069c203a4cadf5a8ffe4c60c557b19014d9
Yosin <ziyoev2612@gmail.com>
2024-09-18 18:44:38 +0700

    Проверка загрузки

--- game_Program/Program.cs
+++ game_Program/Program.cs
@@ -16,6 +16,7 @@ class Program
             .BuildServiceProvider();
 
         // Получаем экземпляры для ввода и вывода
+        //Проверка загрузки
         var input = serviceProvider.GetService<IUserInput>();
         var output = serviceProvider.GetService<IOutput>();
 
